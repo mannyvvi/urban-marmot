4818cffadeaeda42dac1a85efb77cc7370a20a26
mannyvvi <mannyvvi@users.noreply.github.com>
2016-02-23 18:34:02 +0000

    Update Demo.Program.py

--- Demo.Program.py
+++ Demo.Program.py
@@ -1,9 +1,9 @@
-import demo_logger_4
+import demo_logger
 import demo_reader
 
 prod = raw_input("Would you like to run the logger or reader? ",)
 if prod == "logger":
-    demo_logger_4.logger()
+    demo_logger.logger()
 elif prod =="reader":
     demo_reader.reader()
     
